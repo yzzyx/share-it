c92b5f340e78e86284375a1045fe7dbd741c34a3
Elias Norberg <elias@aisle.se>
2021-01-02 12:50:34 +0100

    Only show parts of UI if not connected

--- appwin.c
+++ appwin.c
@@ -20,6 +20,8 @@ struct _ShareitAppWindowPrivate
     GSettings *settings;
     GtkButton *btn_create_session;
     GtkButton *btn_join_session;
+    GtkBox *box_connected; // only shown if we're connected
+    GtkBox *box_reconnect; // only shown if we can reconnect
     connection_t *conn;
 };
 
@@ -35,10 +37,42 @@ static gboolean preferences_clicked(GtkButton *btn) {
     return FALSE;
 }
 
+
+#define STATUS_INITIALIZING 0
+#define STATUS_CONNECTED 1
+#define STATUS_DISCONNECTED 2
+#define STATUS_ERROR 3
+
+static void show_on_status(ShareitAppWindowPrivate  *priv, int status) {
+    if (status == STATUS_CONNECTED) {
+        gtk_widget_show(GTK_WIDGET(priv->box_connected));
+    } else {
+        gtk_widget_hide(GTK_WIDGET(priv->box_connected));
+    }
+
+    if (status == STATUS_ERROR || status == STATUS_DISCONNECTED) {
+        gtk_widget_show(GTK_WIDGET(priv->box_reconnect));
+    } else {
+        gtk_widget_hide(GTK_WIDGET(priv->box_reconnect));
+    }
+}
+
 static int handle_conn_error(char *err, ShareitAppWindow *win) {
+    ShareitAppWindowPrivate *priv = shareit_app_window_get_instance_private(win);
+
     gtk_label_set_text(win->lbl_info, err);
+    show_on_status(priv, STATUS_ERROR);
     return TRUE;
 }
+
+static int handle_conn_connected(char *err, ShareitAppWindow *win) {
+    ShareitAppWindowPrivate *priv = shareit_app_window_get_instance_private(win);
+
+    gtk_label_set_text(win->lbl_info, err);
+    show_on_status(priv, STATUS_CONNECTED);
+    return TRUE;
+}
+
 
 static void shareit_app_window_init (ShareitAppWindow *win) {
     ShareitAppWindowPrivate *priv;
@@ -46,7 +80,7 @@ static void shareit_app_window_init (ShareitAppWindow *win) {
     gtk_widget_init_template(GTK_WIDGET(win));
     priv->settings = g_settings_new ("se.oddbike.shareit");
 
-
+    show_on_status(priv, STATUS_INITIALIZING);
     priv->conn = net_new();
 
     char *servername = g_settings_get_string(priv->settings, "server");
@@ -56,7 +90,7 @@ static void shareit_app_window_init (ShareitAppWindow *win) {
         g_free(txt);
 
         char *err;
-        if (net_connect(priv->conn, servername, &err) < 0) {
+        if (net_connect(priv->conn, "localhost", &err) < 0) {
             txt = g_strdup_printf("Error connecting <b>%s</b>: %s", servername, err);
             gtk_label_set_markup(win->lbl_info, txt);
             g_free(txt);
@@ -65,8 +99,8 @@ static void shareit_app_window_init (ShareitAppWindow *win) {
         gtk_label_set_markup(win->lbl_info, "No server defined, check preferences.");
     }
 
-    net_signal_connect(priv->conn, SIGNAL_ERROR, handle_conn_error, priv);
-
+    net_signal_connect(priv->conn, SIGNAL_ERROR, (handlefunc_t)handle_conn_error, priv);
+    net_signal_connect(priv->conn, SIGNAL_CONNECTED, (handlefunc_t)handle_conn_connected, priv);
     g_free(servername);
 }
 
@@ -77,6 +111,10 @@ static void shareit_app_window_dispose(GObject *object)
 
     win = SHAREIT_APP_WINDOW(object);
     priv = shareit_app_window_get_instance_private(win);
+    if (priv->conn != NULL) {
+        net_free(priv->conn);
+        priv->conn = NULL;
+    }
     g_clear_object(&priv->settings);
     G_OBJECT_CLASS(shareit_app_window_parent_class)->dispose(object);
 }
@@ -87,6 +125,8 @@ static void shareit_app_window_class_init (ShareitAppWindowClass *class) {
     gtk_widget_class_set_template_from_resource(GTK_WIDGET_CLASS(class), "/se/oddbike/shareit/main.ui");
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(class), ShareitAppWindow, btn_create_session);
     gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(class), ShareitAppWindow, btn_join_session);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(class), ShareitAppWindow, box_connected);
+    gtk_widget_class_bind_template_child_private(GTK_WIDGET_CLASS(class), ShareitAppWindow, box_reconnect);
     gtk_widget_class_bind_template_child(GTK_WIDGET_CLASS(class), ShareitAppWindow, lbl_info);
     gtk_widget_class_bind_template_callback (GTK_WIDGET_CLASS (class), preferences_clicked);
 }
